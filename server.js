dd5b77ffd23509e3b694c26f8bd0b453ad3bd2de
chilejay7 <codyburk7@yahoo.com>
2023-11-04 12:52:36 -0600

    notes added to server.js file

--- server.js
+++ server.js
@@ -12,12 +12,14 @@ const PORT = process.env.PORT || 8081;
 app.use(express.static('public'));
 app.use(express.json());
 
+// The GET request initiated by clicking the Get Started button on the home page will provide the notes.html document.
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
     
     console.info(req.method)
 });
 
+// This endpoint responds with the data needed by the menu at /notes to display existing notes.
 app.get('/api/notes', (req, res) => {
     res.json(db);
 })
@@ -27,37 +29,50 @@ app.get('/api/notes/:id', (req, res) => {
     res.send('Here are your notes.')
 });
 
+// This route receives POST requests when the save button is used.
 app.post('/api/notes', (req, res) => {
+
+        // The title and text from the new note created are destructured from the request's body property.  They are used to create a new object.
         const { title, text } = req.body
         console.log(title, text)
+
+        // A new objet is created using the properties from the request.  A uuid is added to each object for use in retrieval of the notes.
         const newNote = {
             title,
             text,
             id: uuid(),
         }
 
+        // The database file is read to retrieve existing notes already in the database.
         fs.readFile('./db/db.json', 'utf8', (err, data) => {
             if (err) {
                 console.log(err) 
             } else {
+                // Notes are first parsed since they have to be stringified for storage.
                 const parsedNotes = JSON.parse(data);
+
+                // The new object from the POST request is pushed to the existing array.
                 parsedNotes.push(newNote);
                 console.log(parsedNotes);
 
+                // The array again has to be stringified for storage in the database file.
                 const newString = JSON.stringify(parsedNotes);
 
+                // The new array is written back to the database.
                 fs.writeFile('./db/db.json', newString, (err) => {
                     err ? console.log(err) : console.log('Data written to db')
                 });
-                res.send('Note successfully written to the database')
+                res.json(db)
             }
         });
 });
 
+// This route will return the user to the home page if they navigate to an endpoint that has not been defined.
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
+// The listener starts the server and will use the port number defined within the PORT variable.
 app.listen(PORT, () => {
     console.log(`Server listenting on Port: ${PORT}`)
 });
